d02fe4e8c995ecbe0643e980b419c3ef923ad196
mehmet <mehmetakz42@gmail.com>
2023-06-28 19:23:14 +0200

    check commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
+    private static String name;
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        name = "Mehmet";
+        System.out.println("Hello" + name);
     }
 }
